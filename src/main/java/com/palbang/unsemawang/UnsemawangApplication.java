62383361e9f36a79e323e479806c22e24c4e2a8f
nardis0321 <nardis0321@gmail.com>
2024-12-27 19:39:01 +0900

    Init: 프로젝트 셋팅

---
+++ src/main/java/com/palbang/unsemawang/UnsemawangApplication.java
@@ -0,0 +1,12 @@
+package com.palbang.unsemawang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsemawangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnsemawangApplication.class, args);
+    }
+}
