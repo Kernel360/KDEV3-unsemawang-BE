292c593dba58fbce7aaea94791b25c5ef5fc362e
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-06 15:45:31 +0900

    Feat: 운세 관련 엔티티 추가

---
+++ src/main/java/com/palbang/unsemawang/fortune/entity/FortuneContent.java
@@ -0,0 +1,58 @@
+package com.palbang.unsemawang.fortune.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Entity
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "fortune_content")
+public class FortuneContent {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 운세 컨텐츠 ID
+
+	//    @ManyToOne(fetch = FetchType.LAZY)
+	//    @JoinColumn(name = "fortune_category_id", nullable = false) // 운세 분야 ID와 다대일 관계
+	//    private FortuneCategory fortuneCategory;
+
+	@Column(name = "fortune_content_name", nullable = false)
+	private String fortuneContentName; // 운세 유형명
+
+	@Column(name = "description", nullable = false, columnDefinition = "TEXT")
+	private String description; // 설명
+
+	@Column(name = "path")
+	private String path;
+
+	@Column(name = "registered_at", nullable = false)
+	private LocalDateTime registeredAt; // 생성일시
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt; // 수정일시
+
+	@Column(name = "is_active", nullable = false)
+	private Boolean isActive = true; // 활성 여부 (기본값: true)
+
+	@Column(name = "is_deleted", nullable = false)
+	private Boolean isDeleted = false; // 삭제 여부 (기본값: false)
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt; // 삭제 일시
+}
---
+++ src/main/java/com/palbang/unsemawang/fortune/entity/FortuneFavorite.java
@@ -0,0 +1,49 @@
+package com.palbang.unsemawang.fortune.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Entity
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "fortune_favorite")
+public class FortuneFavorite {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; // 콘텐츠 찜 ID
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "fortune_content_id", nullable = false) // FortuneContent와 다대일 관계
+	private FortuneContent fortuneContent;
+
+	@Column(name = "member_id", nullable = false)
+	private String memberId; // 일반회원 ID (단순 컬럼)
+
+	@Column(name = "registered_at", nullable = false)
+	private LocalDateTime registeredAt; // 생성일시
+
+	@Column(name = "is_deleted", nullable = false)
+	private Boolean isDeleted = false; // 삭제 여부 (기본값: false)
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt; // 삭제 일시
+}
