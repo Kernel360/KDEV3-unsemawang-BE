7d1cf39891414246a98bcc116faa1d90ea6f1def
yr0202 <yooram0202@naver.com>
2025-01-07 10:36:56 +0900

    Feat: baseEntity 생성

--- src/main/java/com/palbang/unsemawang/UnsemawangApplication.java
+++ src/main/java/com/palbang/unsemawang/UnsemawangApplication.java
@@ -2,11 +2,13 @@ package com.palbang.unsemawang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class UnsemawangApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UnsemawangApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(UnsemawangApplication.class, args);
+	}
 }
