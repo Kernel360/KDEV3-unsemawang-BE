56fea6de18ae13e3332908a6a60f13ea60205e46
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-08 14:18:36 +0900

    Feat: 운세 카테고리 조회 기능 구현

--- src/main/java/com/palbang/unsemawang/fortune/repository/FortuneCategoryRepository.java
+++ src/main/java/com/palbang/unsemawang/fortune/repository/FortuneCategoryRepository.java
@@ -1,5 +1,7 @@
 package com.palbang.unsemawang.fortune.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.palbang.unsemawang.fortune.entity.FortuneCategory;
 
 @Repository
 public interface FortuneCategoryRepository extends JpaRepository<FortuneCategory, Long> {
+	List<FortuneCategory> findAllByIsVisibleIsTrue();
 }
