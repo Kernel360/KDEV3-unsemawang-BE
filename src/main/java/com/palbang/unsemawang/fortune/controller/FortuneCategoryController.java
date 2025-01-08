d61706e3bc1713036798baba1383827653358016
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-08 14:18:36 +0900

    Feat: 운세 카테고리 조회 기능 구현

---
+++ src/main/java/com/palbang/unsemawang/fortune/controller/FortuneCategoryController.java
@@ -0,0 +1,41 @@
+package com.palbang.unsemawang.fortune.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.palbang.unsemawang.common.constants.ResponseCode;
+import com.palbang.unsemawang.common.response.Response;
+import com.palbang.unsemawang.fortune.dto.response.CategoryReadListResponse;
+import com.palbang.unsemawang.fortune.entity.FortuneCategory;
+import com.palbang.unsemawang.fortune.service.FortuneCategoryService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/fortune-category")
+public class FortuneCategoryController {
+
+	private final FortuneCategoryService fortuneCategoryService;
+
+	@GetMapping
+	public ResponseEntity<Response<CategoryReadListResponse>> readList() {
+
+		// 공개 처리 된 카테고리 목록 조회
+		List<FortuneCategory> fortuneCategoryList = fortuneCategoryService.getVisibleList();
+
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(
+				Response.success(
+					ResponseCode.SUCCESS_SEARCH,
+					CategoryReadListResponse.of(fortuneCategoryList)
+				)
+			);
+	}
+}
