42f3122fea8e79a3817959fc1561e127e1a874f9
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-08 13:32:36 +0900

    Chore: 운세 카테고리 패키지 이동

--- src/main/java/com/palbang/unsemawang/fortune/repository/FortuneCategoryRepository.java
+++ src/main/java/com/palbang/unsemawang/fortune/repository/FortuneCategoryRepository.java
@@ -1,9 +1,9 @@
-package com.palbang.unsemawang.member.repository;
+package com.palbang.unsemawang.fortune.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.palbang.unsemawang.member.entity.FortuneCategory;
+import com.palbang.unsemawang.fortune.entity.FortuneCategory;
 
 @Repository
 public interface FortuneCategoryRepository extends JpaRepository<FortuneCategory, Long> {
