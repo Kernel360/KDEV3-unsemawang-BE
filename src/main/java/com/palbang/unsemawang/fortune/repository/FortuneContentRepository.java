c9c181a40402424b373db2d0b954ece15c77cc09
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-09 18:18:12 +0900

    Feat: 이름에 키워드가 포함된 컨텐츠 조회 메서드 추가

--- src/main/java/com/palbang/unsemawang/fortune/repository/FortuneContentRepository.java
+++ src/main/java/com/palbang/unsemawang/fortune/repository/FortuneContentRepository.java
@@ -1,10 +1,16 @@
 package com.palbang.unsemawang.fortune.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.palbang.unsemawang.fortune.entity.FortuneContent;
 
 @Repository
 public interface FortuneContentRepository extends JpaRepository<FortuneContent, Long> {
+
+	@Query("SELECT fc FROM FortuneContent fc WHERE fc.nameEn LIKE %:keyword% OR fc.nameKo LIKE %:keyword%")
+	List<FortuneContent> findByKeyword(String keyword);
 }
