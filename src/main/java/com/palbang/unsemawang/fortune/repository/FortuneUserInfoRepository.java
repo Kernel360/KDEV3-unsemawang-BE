fe98f98a952c9c6ddb7c649ac186013a84327473
yr0202 <yooram0202@naver.com>
2025-01-12 20:30:42 +0900

    Fix: is_deleted=false인 ê것만 조회

--- src/main/java/com/palbang/unsemawang/fortune/repository/FortuneUserInfoRepository.java
+++ src/main/java/com/palbang/unsemawang/fortune/repository/FortuneUserInfoRepository.java
@@ -3,11 +3,13 @@ package com.palbang.unsemawang.fortune.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.palbang.unsemawang.fortune.entity.FortuneUserInfo;
 
 @Repository
 public interface FortuneUserInfoRepository extends JpaRepository<FortuneUserInfo, Long> {
+	@Query("SELECT f FROM FortuneUserInfo f WHERE f.member.id = :id AND f.isDeleted = false")
 	List<FortuneUserInfo> findByMemberId(String id);
 }
