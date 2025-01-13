da539bad4e2b9d2e782b19ea943a076463d9dcd2
yr0202 <yooram0202@naver.com>
2025-01-13 14:28:15 +0900

    Feat: post entity 추가

---
+++ src/main/java/com/palbang/unsemawang/community/constant/Category.java
@@ -0,0 +1,6 @@
+package com.palbang.unsemawang.community.constant;
+
+public enum Category {
+	FREE_BOARD,       // 자유 게시판
+	ANONYMOUS_BOARD
+}
---
+++ src/main/java/com/palbang/unsemawang/community/entity/Post.java
@@ -0,0 +1,81 @@
+package com.palbang.unsemawang.community.entity;
+
+import java.time.LocalDateTime;
+
+import com.palbang.unsemawang.community.constant.Category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Entity
+@Builder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "post")
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "member_id", nullable = false)
+	private String memberId;
+
+	@Column(name = "is_anonymous", nullable = false)
+	@Builder.Default
+	private Boolean isAnonymous = false;
+
+	@Column(nullable = false)
+	private String title = "빈 제목";
+
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String content = "빈 내용";
+
+	@Column(name = "view_count")
+	@Builder.Default
+	private Integer viewCount = 0;
+
+	@Column(name = "like_count")
+	@Builder.Default
+	private Integer likeCount = 0;
+
+	@Column(name = "comment_count")
+	@Builder.Default
+	private Integer commentCount = 0;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Category category;
+
+	@Column(name = "is_visible")
+	@Builder.Default
+	private Boolean isVisible = false;
+
+	@Column(name = "is_deleted")
+	@Builder.Default
+	private Boolean isDeleted = false;
+
+	@Column(name = "registered_at")
+	@Builder.Default
+	private LocalDateTime registeredAt = LocalDateTime.now();
+
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+}
