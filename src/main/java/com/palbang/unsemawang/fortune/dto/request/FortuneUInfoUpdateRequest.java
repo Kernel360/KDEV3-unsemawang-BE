e6697b1ec12cc3beaf6a745751d1dcfb5422d01b
yr0202 <yooram0202@naver.com>
2025-01-13 10:37:06 +0900

    Chore: dto명 변경

--- src/main/java/com/palbang/unsemawang/fortune/dto/request/FortuneUInfoUpdateRequest.java
+++ src/main/java/com/palbang/unsemawang/fortune/dto/request/FortuneUInfoUpdateRequest.java
@@ -2,6 +2,7 @@ package com.palbang.unsemawang.fortune.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -17,35 +18,46 @@ import lombok.Setter;
 @Builder
 @JsonIgnoreProperties
 public class UpdateRequest {
+	@Schema(required = true)
 	@NotBlank(message = "memberId는 필수 값입니다.")
 	private String memberId;
 
+	@Schema(required = true)
 	@NotNull(message = "relationId는 필수 값입니다.")
 	private Long relationId;
 
+	@Schema(required = true)
 	@NotBlank(message = "relationName은 필수 값입니다.")
 	private String relationName;
 
+	@Schema(required = true)
 	@NotBlank(message = "nickname은 필수 값입니다.")
 	private String nickname;
 
+	@Schema(required = true)
 	@NotNull(message = "year는 필수 값입니다.")
 	private int year;
 
+	@Schema(required = true)
 	@NotNull(message = "month는 필수 값입니다.")
 	private int month;
 
+	@Schema(required = true)
 	@NotNull(message = "day는 필수 값입니다.")
 	private int day;
 
-	private int birthtime; // 태어난 시간 (null 허용)
+	@Schema(required = true)
+	private int hour; // 태어난 시간 (null 허용)
 
+	@Schema(required = true)
 	@NotNull(message = "sex는 필수 값입니다.")
 	private char sex;
 
+	@Schema(required = true)
 	@NotNull(message = "youn은 필수 값입니다.")
 	private int youn;
 
+	@Schema(required = true)
 	@NotBlank(message = "solunar는 필수 값입니다.")
 	private String solunar;
 }
