e1d572576ccf611c5f154a428fd706feb6333982
yr0202 <yooram0202@naver.com>
2025-01-13 11:48:43 +0900

    Chore: 삭제 응답 형식 수정

--- src/main/java/com/palbang/unsemawang/fortune/dto/response/DeleteResponseDto.java
+++ src/main/java/com/palbang/unsemawang/fortune/dto/response/DeleteResponseDto.java
@@ -1,13 +1,10 @@
 package com.palbang.unsemawang.fortune.dto.response;
 
-import com.palbang.unsemawang.common.constants.ResponseCode;
-
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class DeleteResponseDto {
-	private final ResponseCode responseCode;
 	private final String message;
 }
--- src/main/java/com/palbang/unsemawang/fortune/service/FortuneUserInfoDeleteService.java
+++ src/main/java/com/palbang/unsemawang/fortune/service/FortuneUserInfoDeleteService.java
@@ -21,36 +21,27 @@ public class FortuneUserInfoDeleteService {
 	private final MemberRepository memberRepository;
 
 	public DeleteResponseDto deleteFortuneUserInfo(String memberId, Long relationId) {
-		try {
-			// 1. Member 조회
-			Member member = memberRepository.findById(memberId)
-				.orElseThrow(() -> new GeneralException(ResponseCode.ERROR_SEARCH, "회원을 찾지 못했습니다."));
 
-			// 2. 사주 정보 조회
-			FortuneUserInfo fortuneUserInfo = fortuneUserInfoRepository.findById(relationId)
-				.orElseThrow(() -> new GeneralException(ResponseCode.ERROR_SEARCH, "해당 사주 정보를 찾지 못했습니다."));
+		// 1. Member 조회
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> new GeneralException(ResponseCode.ERROR_SEARCH, "회원을 찾지 못했습니다."));
 
-			// 3. 삭제 처리
+		// 2. 사주 정보 조회
+		FortuneUserInfo fortuneUserInfo = fortuneUserInfoRepository.findById(relationId)
+			.orElseThrow(() -> new GeneralException(ResponseCode.ERROR_SEARCH, "해당 사주 정보를 찾지 못했습니다."));
+
+		// 조회된 fortuneUserInfo의 idDeleted가 false면 true로 바꾸기
+		if (!fortuneUserInfo.getIsDeleted()) {
 			fortuneUserInfo.deleted();
-
-			// 성공 응답 반환
+		} else {
 			return DeleteResponseDto.builder()
-				.responseCode(ResponseCode.SUCCESS_DELETE)
-				.message("사주 정보 삭제 성공")
-				.build();
-
-		} catch (GeneralException e) {
-			// GeneralException의 경우 실패 코드 반환
-			return DeleteResponseDto.builder()
-				.responseCode(e.getErrorCode())
-				.message(e.getMessage())
-				.build();
-		} catch (Exception e) {
-			// 알 수 없는 예외 처리
-			return DeleteResponseDto.builder()
-				.responseCode(ResponseCode.DEFAULT_INTERNAL_SERVER_ERROR)
-				.message("알 수 없는 서버 오류가 발생했습니다.")
+				.message("이미 삭제된 사주 정보입니다.")
 				.build();
 		}
+
+		// 성공 응답 반환
+		return DeleteResponseDto.builder()
+			.message("사주 정보 삭제 성공")
+			.build();
 	}
 }
