2fcb508cfd5dc640d77233d00b37b105ab48eeda
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-14 22:04:53 +0900

    Feat: 등록 DTO 검증 어노테이션 message 속성 추가

--- src/main/java/com/palbang/unsemawang/community/dto/request/PostRegisterRequest.java
+++ src/main/java/com/palbang/unsemawang/community/dto/request/PostRegisterRequest.java
@@ -20,18 +20,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PostRegisterRequest {
 
-	@Size(min = 1, max = 30)
-	@NotBlank
+	@NotBlank(message = "제목을 입력해 주세요")
+	@Size(min = 1, max = 30, message = "제목은 30자 이내여야 합니다")
 	private String title;
 
-	@Size(min = 1, max = 1000)
-	@NotBlank
+	@NotBlank(message = "내용을 입력해 주세요")
+	@Size(min = 1, max = 1000, message = "내용은 1,000자 이내여야 합니다")
 	private String content;
 
-	@NotNull
+	@NotNull(message = "카테고리를 입력해 주세요")
 	private CommunityCategory category;
 
-	@NotNull
+	@NotNull(message = "익명 여부를 입력해 주세요")
 	private Boolean isAnonymous;
 
 }
