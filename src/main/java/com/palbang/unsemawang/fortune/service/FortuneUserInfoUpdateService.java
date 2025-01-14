4bdbd7130706444bc6bdfe0c7bea020958e71e87
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-14 16:42:08 +0900

    Rename: DTO 이름 변경에 의한 수정

--- src/main/java/com/palbang/unsemawang/fortune/service/FortuneUserInfoUpdateService.java
+++ src/main/java/com/palbang/unsemawang/fortune/service/FortuneUserInfoUpdateService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.palbang.unsemawang.common.constants.ResponseCode;
 import com.palbang.unsemawang.common.exception.GeneralException;
-import com.palbang.unsemawang.fortune.dto.request.FortuneUInfoUpdateRequest;
+import com.palbang.unsemawang.fortune.dto.request.FortuneInfoUpdateRequest;
 import com.palbang.unsemawang.fortune.dto.response.FortuneUserInfoUpdateResponse;
 import com.palbang.unsemawang.fortune.entity.FortuneUserInfo;
 import com.palbang.unsemawang.fortune.entity.UserRelation;
@@ -24,7 +24,7 @@ public class FortuneUserInfoUpdateService {
 	private final MemberRepository memberRepository;
 	private final UserRelationRepository userRelationRepository;
 
-	public FortuneUserInfoUpdateResponse updateFortuneUserInfo(FortuneUInfoUpdateRequest req) {
+	public FortuneUserInfoUpdateResponse updateFortuneUserInfo(FortuneInfoUpdateRequest req) {
 		// Member 조회
 		Member member = memberRepository.findById(req.getMemberId())
 			.orElseThrow(() -> new GeneralException(ResponseCode.ERROR_SEARCH, "회원을 찾지 못했습니다."));
