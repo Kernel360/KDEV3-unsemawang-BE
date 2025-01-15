520103ebe4c1808f6400a1e396d180fa4d101edd
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-15 16:57:30 +0900

    Feat: 게시글 등록 API 명세 어노테이션 추가

--- src/main/java/com/palbang/unsemawang/community/controller/PostController.java
+++ src/main/java/com/palbang/unsemawang/community/controller/PostController.java
@@ -13,9 +13,13 @@ import com.palbang.unsemawang.community.dto.response.PostRegisterResponse;
 import com.palbang.unsemawang.community.service.PostService;
 import com.palbang.unsemawang.oauth2.dto.CustomOAuth2User;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "커뮤니티 게시글")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
@@ -23,9 +27,13 @@ public class PostController {
 
 	private final PostService postService;
 
+	@Operation(
+		description = "커뮤니티 게시글 등록 API 입니다. 인증 토큰이 담긴 쿠키를 직접 보내셔야 테스트가 가능합니다!",
+		summary = "커뮤니티 게시글 등록"
+	)
 	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<PostRegisterResponse> write(
-		@AuthenticationPrincipal CustomOAuth2User auth,
+		@NotNull(message = "인증 객체가 없습니다") @AuthenticationPrincipal CustomOAuth2User auth,
 		@Valid @RequestBody PostRegisterRequest postRegisterRequest
 	) {
 		postRegisterRequest.updateMemberId(auth.getId());
