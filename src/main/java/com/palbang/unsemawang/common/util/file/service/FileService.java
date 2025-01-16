372637ff1c320af646b454235a4ec3231da10aae
nardis0321 <nardis0321@gmail.com>
2025-01-16 16:37:17 +0900

    Feat: 파일 업로드/저장 기능 구현

---
+++ src/main/java/com/palbang/unsemawang/common/util/file/service/FileService.java
@@ -0,0 +1,74 @@
+package com.palbang.unsemawang.common.util.file.service;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.palbang.unsemawang.common.util.file.dto.FileRequest;
+
+public interface FileService {
+
+	/**
+	 * 이미지 업로드
+	 * @param file 업로드 할 이미지 파일
+	 * @param fileRequest ReferenceType과 ReferenceId 입력
+	 */
+	void uploadImage(MultipartFile file, FileRequest fileRequest);
+
+	/**
+	 * 프로필 이미지 url 반환
+	 * @param referenceId 참조된 회원 id, referenceId로 사용된 id
+	 * @return 해당 회원의 프로필 이미지 or 디폴트 프로필 이미지
+	 */
+	String getProfileImgUrl(String referenceId);
+
+	/**
+	 * 파일 url 리스트 반환
+	 * @param fileRequest ReferenceType(참조 테이블명)과 ReferenceId(참조 id) 입력
+	 * @return 파일 url 리스트
+	 */
+	List<String> getFileUrls(FileRequest fileRequest);
+
+	/**
+	 * 이미지 수정 : 이미지는 반드시 하나여야 함
+	 * @param file 수정된 파일
+	 * @param fileRequest 수정할 위치: ReferenceType, ReferenceId
+	 */
+	void updateImage(MultipartFile file, FileRequest fileRequest);
+
+	/**
+	 * 이미지 수정 : 여러 개의 이미지 중 하나를 수정
+	 * @param file 수정된 파일
+	 * @param updateTargetId 수정할 파일 id
+	 */
+	void updateImageById(MultipartFile file, Long updateTargetId);
+
+	/**
+	 * 이미지 수정 : 여러개의 이미지 전체 삭제 후 전체 추가
+	 * @param files 이미지 리스트
+	 * @param fileRequest 수정할 위치
+	 */
+	void updateImagesAtOnce(List<MultipartFile> files, FileRequest fileRequest);
+
+	/**
+	 * 파일 삭제
+	 * @param fileRequest 삭제할 파일 위치:  ReferenceType, ReferenceId
+	 * @return 삭제 여부
+	 */
+	boolean deleteFile(FileRequest fileRequest);
+
+	/**
+	 * 파일 삭제 여러개
+	 * @param fileRequest 삭제할 파일 위치:  ReferenceType, ReferenceId
+	 * @return 삭제 여부
+	 */
+	boolean deleteFiles(FileRequest fileRequest);
+
+	/**
+	 * 파일 삭제 아이디로
+	 * @param fileId 삭제할 파일 아이디
+	 * @return 삭제 여부
+	 */
+	boolean deleteFileById(Long fileId);
+
+}
