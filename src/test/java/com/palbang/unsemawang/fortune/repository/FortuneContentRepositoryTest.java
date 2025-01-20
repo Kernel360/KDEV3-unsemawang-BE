0e74d1e23d2a2d67406dd1e71d438647f43d23f8
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-20 12:33:53 +0900

    Test: queryDsl 관련 빈 등록하도록 설정

--- src/test/java/com/palbang/unsemawang/fortune/repository/FortuneContentRepositoryTest.java
+++ src/test/java/com/palbang/unsemawang/fortune/repository/FortuneContentRepositoryTest.java
@@ -9,14 +9,15 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.dao.DataIntegrityViolationException;
 
+import com.palbang.unsemawang.config.QueryDslConfig;
 import com.palbang.unsemawang.fortune.entity.FortuneContent;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(QueryDslConfig.class)
 class FortuneContentRepositoryTest {
 	@Autowired
 	FortuneContentRepository fortuneContentRepository;
