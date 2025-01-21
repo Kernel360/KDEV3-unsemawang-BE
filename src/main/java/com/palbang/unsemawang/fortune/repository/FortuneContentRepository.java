d04b59200071840e98b52f64fc8e6c4c9859a387
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-21 14:54:35 +0900

    Fix: 공개 처리된 컨텐츠만 조회되도록 수정

--- src/main/java/com/palbang/unsemawang/fortune/repository/FortuneContentRepository.java
+++ src/main/java/com/palbang/unsemawang/fortune/repository/FortuneContentRepository.java
@@ -14,6 +14,6 @@ public interface FortuneContentRepository extends JpaRepository<FortuneContent, 
 	@Query("SELECT fc FROM FortuneContent fc WHERE fc.nameEn LIKE %:keyword% OR fc.nameKo LIKE %:keyword%")
 	List<FortuneContent> findByKeyword(String keyword);
 
-	@Query("SELECT fc FROM FortuneContent fc WHERE fc.fortuneCategory.nameEn = :categoryName")
+	@Query("SELECT fc FROM FortuneContent fc WHERE fc.fortuneCategory.nameEn = :categoryName AND fc.isVisible = true")
 	List<FortuneContent> findAllByFortuneCategory(String categoryName);
 }
