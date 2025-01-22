79919cbd727fa862378243989b95c7712bacbbda
yr0202 <yooram0202@naver.com>
2025-01-22 23:34:45 +0900

    Feat: 댓글 등록 수정 dto 생성

---
+++ src/main/java/com/palbang/unsemawang/community/dto/request/CommentUpdateRequest.java
@@ -0,0 +1,23 @@
+package com.palbang.unsemawang.community.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentUpdateRequest {
+	@Schema(required = true, description = "댓글 내용")
+	@NotBlank
+	@Size(min = 1, max = 250)
+	private String content;
+
+	@Schema(required = false, description = "익명 여부", defaultValue = "false")
+	private Boolean isAnonymous;
+}
