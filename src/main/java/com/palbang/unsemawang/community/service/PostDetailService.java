ba5f2ce80a13e8f246dabe7547e7dc454e51ba85
yschoi123 <cysdl5604@inu.ac.kr>
2025-01-23 11:01:56 +0900

    Feat: 게시글 조회시 작성자 프로필 및 게시글 이미지를 읽어도록 로직 추가

--- src/main/java/com/palbang/unsemawang/community/service/PostDetailService.java
+++ src/main/java/com/palbang/unsemawang/community/service/PostDetailService.java
@@ -1,23 +1,29 @@
 package com.palbang.unsemawang.community.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.palbang.unsemawang.common.constants.ResponseCode;
 import com.palbang.unsemawang.common.exception.GeneralException;
+import com.palbang.unsemawang.common.util.file.dto.FileReferenceType;
+import com.palbang.unsemawang.common.util.file.dto.FileRequest;
+import com.palbang.unsemawang.common.util.file.service.FileService;
 import com.palbang.unsemawang.community.dto.response.PostDetailResponse;
 import com.palbang.unsemawang.community.entity.Post;
 import com.palbang.unsemawang.community.repository.PostRepository;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Service
 public class PostDetailService {
 
+	private final FileService fileService;
 	private final PostRepository postRepository;
 
-	@Transactional
+	@Transactional(readOnly = true)
 	public PostDetailResponse getPostDetail(String memberId, Long postId) {
 		Post post = postRepository.findById(postId)
 			.orElseThrow(() -> new GeneralException(ResponseCode.RESOURCE_NOT_FOUND));
@@ -27,25 +33,32 @@ public class PostDetailService {
 			throw new GeneralException(ResponseCode.FORBIDDEN); // 적절한 응답 코드 사용
 		}
 
-		// DB 레벨에서 조회수 증가
-		incrementViewCount(postId);
+		// 게시글에 포함된 이미지 리스트 조회
+		FileRequest fileRequest = FileRequest.of(FileReferenceType.COMMUNITY_BOARD, postId);
+		List<String> imageUrls = fileService.getFileUrls(fileRequest);
 
-		return toResponseDto(post);
+		// 작성자의 프로필 이미지 조회
+		String profileImage = fileService.getProfileImgUrl(memberId);
+
+		return toResponseDto(post, imageUrls, profileImage);
 	}
 
 	// 조회수 증가 - DB 레벨 처리
-	private void incrementViewCount(Long postId) {
+	@Transactional
+	public void incrementViewCount(Long postId) {
 		postRepository.incrementViewCount(postId);
 	}
 
 	// Dto 컨버터
-	private PostDetailResponse toResponseDto(Post post) {
+	private PostDetailResponse toResponseDto(Post post, List<String> imageUrls, String profileImageUrl) {
 		return PostDetailResponse.builder()
 			.id(post.getId())
 			.memberId(post.getMember().getId())
 			.title(post.getTitle())
 			.content(post.getContent())
 			.nickname(post.getIsAnonymous() ? "익명" : post.getMember().getNickname())
+			.profileImageUrl(profileImageUrl)
+			.postImageUrls(imageUrls)
 			.isAnonymous(post.getIsAnonymous())
 			.isVisible(post.getIsVisible())
 			.viewCount(post.getViewCount())
