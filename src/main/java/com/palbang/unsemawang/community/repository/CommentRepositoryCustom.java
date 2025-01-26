363f0b927de701c36705a302d8536f8263ce9f1b
yr0202 <yooram0202@naver.com>
2025-01-27 02:35:28 +0900

    Feat: 부모 댓글, 자식 댓글 조회 분리

--- src/main/java/com/palbang/unsemawang/community/repository/CommentRepositoryCustom.java
+++ src/main/java/com/palbang/unsemawang/community/repository/CommentRepositoryCustom.java
@@ -1,8 +1,12 @@
 package com.palbang.unsemawang.community.repository;
 
+import java.util.List;
+
 import com.palbang.unsemawang.common.util.pagination.LongCursorResponse;
 import com.palbang.unsemawang.community.entity.Comment;
 
 public interface CommentRepositoryCustom {
 	LongCursorResponse<Comment> findCommentsByPostIdAndCursor(Long postId, Long cursorKey, int size);
+
+	List<Comment> findChildCommentsByParentIds(List<Long> parentIds);
 }
