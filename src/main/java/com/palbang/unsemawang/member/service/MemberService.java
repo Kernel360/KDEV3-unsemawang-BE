a4ea557be2de0d532e87b14b1c571d5d9883c75a
superwalk <sukmo88@naver.com>
2025-01-27 19:19:30 +0900

    fix: 비회원(임시회원) -> 회원 등급 변경시 JWT재발급

--- src/main/java/com/palbang/unsemawang/member/service/MemberService.java
+++ src/main/java/com/palbang/unsemawang/member/service/MemberService.java
@@ -1,16 +1,22 @@
 package com.palbang.unsemawang.member.service;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.palbang.unsemawang.common.constants.ResponseCode;
 import com.palbang.unsemawang.common.exception.GeneralException;
+import com.palbang.unsemawang.jwt.JWTUtil;
+import com.palbang.unsemawang.member.constant.MemberRole;
 import com.palbang.unsemawang.member.dto.MemberProfileDto;
 import com.palbang.unsemawang.member.dto.SignupExtraInfoRequest;
 import com.palbang.unsemawang.member.dto.request.UpdateMemberRequest;
 import com.palbang.unsemawang.member.dto.response.UpdateMemberResponse;
 import com.palbang.unsemawang.member.entity.Member;
 import com.palbang.unsemawang.member.repository.MemberRepository;
+import com.palbang.unsemawang.oauth2.dto.CustomOAuth2User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberService {
 	private final MemberRepository memberRepository;
+	private final JWTUtil jwtUtil;
 
 	// nickname 중복 여부 확인
 	public void duplicateNicknameCheck(String nickname) {
@@ -42,6 +49,7 @@ public class MemberService {
 		//회원가입 추가정보 입력 여부 값 변경
 		member = member.toBuilder()
 			.isJoin(true)
+			.role(MemberRole.GENERAL)
 			.build();
 
 		memberRepository.save(member);
@@ -87,5 +95,29 @@ public class MemberService {
 		memberRepository.save(updateMember);
 
 		return new UpdateMemberResponse(updateMember.getNickname(), updateMember.getDetailBio());
+	}
+
+	//jwt 발급후 쿠키에 넣어서 전달
+	public HttpHeaders addJwtToCookie(@AuthenticationPrincipal CustomOAuth2User auth){
+		HttpHeaders headers = new HttpHeaders();
+		String token = jwtUtil.createJTwt(auth.getId(), auth.getEmail(),"GENERAL",60 * 60 * 1000L * 24 * 3);
+		// 쿠키 추가
+		ResponseCookie responseCookie = createResponseCookie("Authorization", token, 60 * 60 * 24 * 3);
+
+		headers.add(HttpHeaders.SET_COOKIE, responseCookie.toString());
+
+		return headers;
+	}
+
+	private ResponseCookie createResponseCookie(String key, String value, int maxAge) {
+		// ResponseCookie 생성 시 SameSite=None 설정
+		return ResponseCookie.from(key, value)
+			.path("/")
+			.domain("unsemawang.com") // 도메인 지정 (필요한 경우 설정)
+			.sameSite("None") // SameSite=None 설정
+			.secure(true)     // HTTPS에서만 전송
+			.httpOnly(true)   // JavaScript 접근 불가
+			.maxAge(maxAge)   // 만료 시간 설정
+			.build();
 	}
 }
