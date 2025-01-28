2ac213e8246815f1b83e26c0980723a3e52b7fd6
yr0202 <yooram0202@naver.com>
2025-01-28 14:54:17 +0900

    Fix: 본인 댓글 여부 추가

--- src/main/java/com/palbang/unsemawang/community/dto/response/CommentReadResponse.java
+++ src/main/java/com/palbang/unsemawang/community/dto/response/CommentReadResponse.java
@@ -17,6 +17,9 @@ public class CommentReadResponse {
 	@Schema(required = true)
 	private Long commentId;
 
+	@Schema(required = false)
+	private Boolean isMyComment;
+
 	@Schema(required = true)
 	private String nickname; // 익명 게시판 -> 익명 이름, 자유 게시판 -> 사용자 별명
 
@@ -26,7 +29,7 @@ public class CommentReadResponse {
 	@Schema(required = true)
 	private LocalDateTime registeredAt;
 
-	@Schema(required = false)
+	@Schema(required = true)
 	private int repliesCount;
 
 	@Schema(required = true)
