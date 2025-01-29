7da009595ae9e63d48b5e75b4210889c9ffa67fb
yr0202 <yooram0202@naver.com>
2025-01-29 11:39:36 +0900

    Fix: 토큰 검증 추가

--- src/main/java/com/palbang/unsemawang/fortune/controller/FortuneUserInfoReadController.java
+++ src/main/java/com/palbang/unsemawang/fortune/controller/FortuneUserInfoReadController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.palbang.unsemawang.common.constants.ResponseCode;
+import com.palbang.unsemawang.common.exception.GeneralException;
 import com.palbang.unsemawang.fortune.dto.response.FortuneUserInfoReadResponseDto;
 import com.palbang.unsemawang.fortune.service.FortuneUserInfoReadService;
 import com.palbang.unsemawang.oauth2.dto.CustomOAuth2User;
@@ -39,6 +41,10 @@ public class FortuneUserInfoReadController {
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<FortuneUserInfoReadResponseDto>> getUserInfoList(
 		@AuthenticationPrincipal CustomOAuth2User auth) {
+
+		if (auth == null || auth.getId() == null) {
+			throw new GeneralException(ResponseCode.EMPTY_TOKEN);
+		}
 
 		List<FortuneUserInfoReadResponseDto> list = readService.fortuneInfoListRead(auth.getId());
 
