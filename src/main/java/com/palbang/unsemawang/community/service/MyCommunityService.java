aeb6f3ac456c4acc8004dde66f627f84a9a22904
yr0202 <yooram0202@naver.com>
2025-01-31 19:21:42 +0900

    Feat: 내가 쓴 게시글 조회

--- src/main/java/com/palbang/unsemawang/community/service/MyCommunityService.java
+++ src/main/java/com/palbang/unsemawang/community/service/MyCommunityService.java
@@ -7,8 +7,12 @@ import com.palbang.unsemawang.common.constants.ResponseCode;
 import com.palbang.unsemawang.common.exception.GeneralException;
 import com.palbang.unsemawang.common.util.pagination.CursorRequest;
 import com.palbang.unsemawang.common.util.pagination.LongCursorResponse;
+import com.palbang.unsemawang.community.constant.CommunityCategory;
+import com.palbang.unsemawang.community.constant.Sortingtype;
 import com.palbang.unsemawang.community.dto.response.MyCommentsReadResponse;
+import com.palbang.unsemawang.community.dto.response.PostListResponse;
 import com.palbang.unsemawang.community.repository.CommentRepository;
+import com.palbang.unsemawang.community.repository.PostRepository;
 import com.palbang.unsemawang.member.entity.Member;
 import com.palbang.unsemawang.member.repository.MemberRepository;
 
@@ -19,17 +23,25 @@ import lombok.RequiredArgsConstructor;
 public class MyCommunityService {
 	private final MemberRepository memberRepository;
 	private final CommentRepository commentRepository;
+	private final PostRepository postRepository;
 
-	@Transactional
+	@Transactional(readOnly = true)
 	public LongCursorResponse<MyCommentsReadResponse> commentListRead(CursorRequest<Long> cursorRequest,
 		String memberId) {
 
 		Member member = memberRepository.findById(memberId)
 			.orElseThrow(() -> new GeneralException(ResponseCode.NOT_EXIST_MEMBER_ID));
 
-		LongCursorResponse<MyCommentsReadResponse> comments = commentRepository.findMyCommentsByCursor(memberId,
-			cursorRequest);
+		return commentRepository.findMyCommentsByCursor(memberId, cursorRequest);
+	}
 
-		return comments;
+	@Transactional(readOnly = true)
+	public LongCursorResponse<PostListResponse> postListRead(CursorRequest<Long> cursorRequest,
+		String memberId, CommunityCategory category, Sortingtype sort) {
+
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(() -> new GeneralException(ResponseCode.NOT_EXIST_MEMBER_ID));
+
+		return postRepository.findMyPostsByCursor(memberId, cursorRequest, category, sort);
 	}
 }
