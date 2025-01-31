72949d2845cc465bec54855c86cab8bf18bed951
superwalk <sukmo88@naver.com>
2025-01-31 11:51:33 +0900

    fix: 본인사주조회시 param 추가

--- src/main/java/com/palbang/unsemawang/fortune/repository/FortuneUserInfoRepository.java
+++ src/main/java/com/palbang/unsemawang/fortune/repository/FortuneUserInfoRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.palbang.unsemawang.fortune.entity.FortuneUserInfo;
@@ -16,6 +17,6 @@ public interface FortuneUserInfoRepository extends JpaRepository<FortuneUserInfo
 	@Query("SELECT f FROM FortuneUserInfo f " +
 		"JOIN f.relation r " +
 		"WHERE f.member.id = :id AND r.relationName = :relationName AND f.isDeleted = false")
-	List<FortuneUserInfo> findByMemberIdAndRelation(String id, String relationName);
+	List<FortuneUserInfo> findByMemberIdAndRelation(@Param("id") String id, @Param("relationName") String relationName);
 
 }
