4d9a0ae95ef96f77eae6034450ad7f0de10de480
yr0202 <yooram0202@naver.com>
2025-01-31 20:13:22 +0900

    Fix: 새로 만든 dto로 타입 변경

--- src/main/java/com/palbang/unsemawang/community/repository/PostRepositoryCustom.java
+++ src/main/java/com/palbang/unsemawang/community/repository/PostRepositoryCustom.java
@@ -4,9 +4,9 @@ import com.palbang.unsemawang.common.util.pagination.CursorRequest;
 import com.palbang.unsemawang.common.util.pagination.LongCursorResponse;
 import com.palbang.unsemawang.community.constant.CommunityCategory;
 import com.palbang.unsemawang.community.constant.Sortingtype;
-import com.palbang.unsemawang.community.dto.response.PostListResponse;
+import com.palbang.unsemawang.community.dto.response.PostProjectionDto;
 
 public interface PostRepositoryCustom {
-	LongCursorResponse<PostListResponse> findMyPostsByCursor(String memberId, CursorRequest<Long> cursorRequest,
+	LongCursorResponse<PostProjectionDto> findMyPostsByCursor(String memberId, CursorRequest<Long> cursorRequest,
 		CommunityCategory category, Sortingtype sortType);
 }
