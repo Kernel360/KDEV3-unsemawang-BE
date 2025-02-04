61636b94e53ce9b047cb5cf5f12324e0b58e99c2
yr0202 <yooram0202@naver.com>
2025-02-04 11:27:08 +0900

    Chore: style에 맞게 수정

--- src/main/java/com/palbang/unsemawang/community/controller/CommentController.java
+++ src/main/java/com/palbang/unsemawang/community/controller/CommentController.java
@@ -37,7 +37,8 @@ public class CommentController {
 
 	@Operation(
 		description = "커서 기반 페이징 처리된 댓글 조회 기능입니다. 처음 조회시에는 cursorKey를 null로 보내시면 됩니다",
-		summary = "댓글 조회 (커서 기반 페이징)")
+		summary = "댓글 조회 (커서 기반 페이징)"
+	)
 	@GetMapping
 	public ResponseEntity<LongCursorResponse<CommentReadResponse>> getAllCommentsByPostId(
 		@AuthenticationPrincipal CustomOAuth2User auth,
@@ -63,12 +64,14 @@ public class CommentController {
 
 	@Operation(
 		description = "해당 게시글에 댓글을 등록할 수 있습니다. 댓글과 대댓글까지 작성할 수 있습니다.",
-		summary = "댓글/대댓글 등록")
+		summary = "댓글/대댓글 등록"
+	)
 	@PostMapping
 	public ResponseEntity<Void> registerCommentByPostId(
 		@AuthenticationPrincipal CustomOAuth2User auth,
 		@RequestParam Long postId,
 		@RequestBody @Valid CommentRegisterRequest request) {
+
 		if (auth == null || auth.getId() == null) {
 			throw new GeneralException(ResponseCode.EMPTY_TOKEN);
 		}
@@ -80,12 +83,14 @@ public class CommentController {
 
 	@Operation(
 		description = "해당 게시글에 본인이 작성한 댓글/대댓글을 수정할 수 있습니다.",
-		summary = "댓글/대댓글 수정")
+		summary = "댓글/대댓글 수정"
+	)
 	@PutMapping("/{commentId}")
 	public ResponseEntity<Void> updateCommentByPostId(
 		@AuthenticationPrincipal CustomOAuth2User auth,
 		@PathVariable Long commentId,
 		@RequestBody @Valid CommentUpdateRequest request) {
+
 		if (auth == null || auth.getId() == null) {
 			throw new GeneralException(ResponseCode.EMPTY_TOKEN);
 		}
@@ -97,7 +102,8 @@ public class CommentController {
 
 	@Operation(
 		description = "내가 작성한 댓글을 삭제하는 기능입니다",
-		summary = "댓글 삭제")
+		summary = "댓글 삭제"
+	)
 	@DeleteMapping("/{commentId}")
 	public ResponseEntity<Void> deleteCommentByPostId(
 		@AuthenticationPrincipal CustomOAuth2User auth,
