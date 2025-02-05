5ecd9ce41522f17ac7cb0ae2005ced57abc98c32
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-05 16:41:58 +0900

    Fix: 로깅 어노테이션 제거

--- src/main/java/com/palbang/unsemawang/member/dto/SignupExtraInfoRequest.java
+++ src/main/java/com/palbang/unsemawang/member/dto/SignupExtraInfoRequest.java
@@ -16,9 +16,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Builder
 @Getter
 @NoArgsConstructor
