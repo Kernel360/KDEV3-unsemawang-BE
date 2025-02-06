fdfd282fd99e522c1a973862f94334cecc70e92f
yr0202 <yooram0202@naver.com>
2025-02-06 10:52:51 +0900

    Feat: 오행 한자 추가

--- src/main/java/com/palbang/unsemawang/chemistry/constant/FiveElements.java
+++ src/main/java/com/palbang/unsemawang/chemistry/constant/FiveElements.java
@@ -9,16 +9,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum FiveElements {
-	PLUS_TREE("목", "甲", "갑", true),
-	MINUS_TREE("목", "乙", "을", false),
-	PLUS_FIRE("화", "丙", "병", true),
-	MINUS_FIRE("화", "丁", "정", false),
-	PLUS_SOIL("토", "戊", "무", true),
-	MINUS_SOIL("토", "己", "기", false),
-	PLUS_GOLD("금", "庚", "경", true),
-	MINUS_GOLD("금", "辛", "신", false),
-	PLUS_WATER("수", "壬", "임", true),
-	MINUS_WATER("수", "癸", "계", false);
+	PLUS_TREE("목", "木", "甲", "갑", true),
+	MINUS_TREE("목", "木", "乙", "을", false),
+	PLUS_FIRE("화", "火", "丙", "병", true),
+	MINUS_FIRE("화", "火", "丁", "정", false),
+	PLUS_SOIL("토", "土", "戊", "무", true),
+	MINUS_SOIL("토", "土", "己", "기", false),
+	PLUS_GOLD("금", "金", "庚", "경", true),
+	MINUS_GOLD("금", "金", "辛", "신", false),
+	PLUS_WATER("수", "水", "壬", "임", true),
+	MINUS_WATER("수", "水", "癸", "계", false);
 
 	private static final Map<String, FiveElements> lookup = new HashMap<>();
 
@@ -29,7 +29,8 @@ public enum FiveElements {
 	}
 
 	private final String element;
-	private final String hanja;
+	private final String chinese;
+	private final String dayGan;
 	private final String reading;
 	private final boolean isYang;
 
