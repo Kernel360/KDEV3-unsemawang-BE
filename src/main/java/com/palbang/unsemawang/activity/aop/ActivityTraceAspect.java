ffc31226255823415fb469e56e7e1bc8d2a874e3
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-07 20:53:36 +0900

    Feat: api 요청마다 활동 리스트 갱신
    # Conflicts:
    #	src/main/java/com/palbang/unsemawang/activity/aop/ActivityTraceAspect.java

--- src/main/java/com/palbang/unsemawang/activity/aop/ActivityTraceAspect.java
+++ src/main/java/com/palbang/unsemawang/activity/aop/ActivityTraceAspect.java
@@ -1,10 +1,12 @@
 package com.palbang.unsemawang.activity.aop;
 
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import com.palbang.unsemawang.activity.entity.ActiveMember;
 import com.palbang.unsemawang.activity.service.ActiveMemberService;
 import com.palbang.unsemawang.oauth2.dto.CustomOAuth2User;
 
@@ -22,7 +24,7 @@ public class ActivityTraceAspect {
 	/**
 	 * 회원이 API 요청을 수행한 후 활동 내역을 Redis에 저장
 	 */
-	// @AfterReturning("execution(* com.palbang.unsemawang..controller..*(..)) && !@annotation(com.palbang.unsemawang.activity.aop.NoTracking)")
+	@AfterReturning("execution(* com.palbang.unsemawang..controller..*(..)) && !@annotation(com.palbang.unsemawang.activity.aop.NoTracking)")
 	public void trackUserActivity() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
@@ -31,19 +33,10 @@ public class ActivityTraceAspect {
 			return;
 		}
 
-		Object principal = auth.getPrincipal(); // Principal 확인
+		String memberId = ((CustomOAuth2User)auth.getPrincipal()).getId();
 
-		String memberId;
-		if (principal instanceof CustomOAuth2User) {
-			memberId = ((CustomOAuth2User)principal).getId();
-		} else if (principal instanceof String) {
-			memberId = (String)principal; // Principal이 String인 경우
-		} else {
-			log.error("예상치 못한 Principal 타입: {}", principal.getClass().getName());
-			return;
-		}
-
-		log.info("회원 활동 기록: {}", memberId);
-		activeMemberService.saveAndUpdateActiveMember(memberId);
+		// 레디스 활동 리스트 갱신
+		ActiveMember savedActiveMember = activeMemberService.saveAndUpdateActiveMember(memberId);
+		log.info("저장된 회원 활동 정보: {}", savedActiveMember);
 	}
 }
