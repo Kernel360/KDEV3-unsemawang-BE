24ce00b8da9dd4482994f216ffa3aa11b83c517d
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-07 22:27:16 +0900

    Feat: 타입 필드 추가

--- src/main/java/com/palbang/unsemawang/chat/constant/SenderType.java
+++ src/main/java/com/palbang/unsemawang/chat/constant/SenderType.java
@@ -1,12 +1,19 @@
 package com.palbang.unsemawang.chat.constant;
 
 public enum SenderType {
-	SELF("본인"),
-	OTHER("상대방");
+	SELF("self", "본인"),
+	OTHER("other", "상대방");
 
+	private String type;
 	private String desc;
 
-	SenderType(String desc) {
+	SenderType(String type, String desc) {
 		this.desc = desc;
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return this.type;
 	}
 }
