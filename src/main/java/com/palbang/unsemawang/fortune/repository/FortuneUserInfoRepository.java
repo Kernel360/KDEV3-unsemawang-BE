6e5e9684a6b124f418e2e197a9bc4121c4f2ba2a
yr0202 <yooram0202@naver.com>
2025-02-07 10:24:23 +0900

    Feat: "본인" 사주 정보 존재 확인 쿼리

--- src/main/java/com/palbang/unsemawang/fortune/repository/FortuneUserInfoRepository.java
+++ src/main/java/com/palbang/unsemawang/fortune/repository/FortuneUserInfoRepository.java
@@ -19,4 +19,5 @@ public interface FortuneUserInfoRepository extends JpaRepository<FortuneUserInfo
 		"WHERE f.member.id = :id AND r.relationName = :relationName AND f.isDeleted = false")
 	List<FortuneUserInfo> findByMemberIdAndRelation(@Param("id") String id, @Param("relationName") String relationName);
 
+	boolean existsByMemberIdAndRelationId(String memberId, Long relationId);
 }
