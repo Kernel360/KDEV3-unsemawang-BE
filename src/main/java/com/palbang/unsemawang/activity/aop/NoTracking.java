744fc4e08fef496073cd06fc04d027b5ea0f2180
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-07 20:52:34 +0900

    Feat: 사용자 활동 추적 제외용 어노테이션 추가

---
+++ src/main/java/com/palbang/unsemawang/activity/aop/NoTracking.java
@@ -0,0 +1,11 @@
+package com.palbang.unsemawang.activity.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoTracking {
+}
