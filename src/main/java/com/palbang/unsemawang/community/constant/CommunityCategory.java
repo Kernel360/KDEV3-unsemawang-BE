2612ac38f4732eccddd8d58202073ea352ce0172
yr0202 <yooram0202@naver.com>
2025-02-09 18:01:47 +0900

    Feat: 운세공유 카테고리 추가

--- src/main/java/com/palbang/unsemawang/community/constant/CommunityCategory.java
+++ src/main/java/com/palbang/unsemawang/community/constant/CommunityCategory.java
@@ -2,5 +2,6 @@ package com.palbang.unsemawang.community.constant;
 
 public enum CommunityCategory {
 	FREE_BOARD,             // 자유 게시판
-	ANONYMOUS_BOARD;        // 익명 게시판
+	ANONYMOUS_BOARD,        // 익명 게시판
+	SHARE_FORTUNE_BOARD;            // 운세 공유 게시판
 }
