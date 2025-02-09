f10a1511c868db1f968f1501f06f91ed293ef6e6
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-09 11:41:52 +0900

    Fix: 응답 스키마 지정 제거

--- src/main/java/com/palbang/unsemawang/chemistry/controller/RecommendController.java
+++ src/main/java/com/palbang/unsemawang/chemistry/controller/RecommendController.java
@@ -8,8 +8,6 @@ import com.palbang.unsemawang.chemistry.dto.response.ChemistryRecommendResponse;
 import com.palbang.unsemawang.oauth2.dto.CustomOAuth2User;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -20,8 +18,7 @@ public interface RecommendController {
 		description = "오행 기반 궁합이 잘맞는 회원 추천 리스트를 조회합니다",
 		summary = "오행 기반 매칭 결과 조회"
 	)
-	@ApiResponse(responseCode = "200", description = "궁합 좋은 회원 추천 리스트 조회 성공",
-		content = @Content(schema = @Schema(implementation = ChemistryRecommendResponse.class)))
+	@ApiResponse(responseCode = "200", description = "궁합 좋은 회원 추천 리스트 조회 성공")
 	ResponseEntity<List<ChemistryRecommendResponse>> recommendChemistryMember(
 		CustomOAuth2User user
 	);
