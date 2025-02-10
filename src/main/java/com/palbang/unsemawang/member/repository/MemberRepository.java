01dd156586ef2b90f84b7680c25dc5d02d94882a
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-10 19:52:20 +0900

    Feat:  특정 날짜를 기점으로 활동한 회원 리스트 조회 메서드 추가

--- src/main/java/com/palbang/unsemawang/member/repository/MemberRepository.java
+++ src/main/java/com/palbang/unsemawang/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.palbang.unsemawang.member.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,6 +34,9 @@ public interface MemberRepository extends JpaRepository<Member, String> {
 		    WHERE f.relation.id = 1
 		""")
 	List<MemberWithDayGanDto> findAllMembersWithDayGan();
+
+	// 특정 날짜를 기점으로 활동한 회원 리스트 조회
+	List<Member> findAllByLastActivityAtAfter(LocalDateTime lastActivityAt);
 
 	@EntityGraph(attributePaths = {"nickname", "profileUrl"})
 	@Query("SELECT m FROM Member m WHERE m.id = :id")
