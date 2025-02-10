b9513ea5a23dfa9972304beb34b7a3971300925f
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-10 19:55:57 +0900

    Feat: api 요청마다 rdb에 마지막 활동 일시 갱신하도록 변경

--- src/main/java/com/palbang/unsemawang/activity/aop/ActivityTraceAspect.java
+++ src/main/java/com/palbang/unsemawang/activity/aop/ActivityTraceAspect.java
@@ -2,12 +2,13 @@ package com.palbang.unsemawang.activity.aop;
 
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import com.palbang.unsemawang.activity.entity.ActiveMember;
-import com.palbang.unsemawang.activity.service.ActiveMemberService;
+import com.palbang.unsemawang.activity.service.ActiveMemberRedisService;
+import com.palbang.unsemawang.member.service.MemberActivityService;
 import com.palbang.unsemawang.oauth2.dto.CustomOAuth2User;
 
 import lombok.RequiredArgsConstructor;
@@ -19,24 +20,55 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class ActivityTraceAspect {
 
-	private final ActiveMemberService activeMemberService;
+	private final RedisTemplate redisTemplate;
+	private final ActiveMemberRedisService activeMemberService;
+	private final MemberActivityService memberActivityService;
+
+	public boolean isRedisConnected() {
+		try {
+			String pingResult = redisTemplate.getConnectionFactory().getConnection().ping();
+			return "PONG".equalsIgnoreCase(pingResult);
+		} catch (Exception e) {
+			return false;
+		}
+	}
 
 	/**
 	 * 회원이 API 요청을 수행한 후 활동 내역을 Redis에 저장
+	 * 레디스 연결될 때 까지 주석 처리하겠습니다
 	 */
+	// @AfterReturning("execution(* com.palbang.unsemawang..controller..*(..)) && !@annotation(com.palbang.unsemawang.activity.aop.NoTracking)")
+	// public void trackUserActivity() {
+	//
+	// 	if (!isRedisConnected()) {
+	// 		log.error("레디스 연결 정보 없음");
+	// 		return;
+	// 	}
+	//
+	// 	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	//
+	// 	if (auth == null || !auth.isAuthenticated()) {
+	// 		log.warn("인증 정보가 없습니다");
+	// 		return;
+	// 	}
+	//
+	// 	String memberId = ((CustomOAuth2User)auth.getPrincipal()).getId();
+	//
+	// 	// 레디스 활동 리스트 갱신
+	// 	ActiveMember savedActiveMember = activeMemberService.saveAndUpdateActiveMember(memberId);
+	// 	log.info("저장된 회원 활동 정보: {}", savedActiveMember);
+	// }
 	@AfterReturning("execution(* com.palbang.unsemawang..controller..*(..)) && !@annotation(com.palbang.unsemawang.activity.aop.NoTracking)")
-	public void trackUserActivity() {
+	public void updateUserActivity() {
+
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
 		if (auth == null || !auth.isAuthenticated()) {
 			log.warn("인증 정보가 없습니다");
 			return;
 		}
-
 		String memberId = ((CustomOAuth2User)auth.getPrincipal()).getId();
 
-		// 레디스 활동 리스트 갱신
-		ActiveMember savedActiveMember = activeMemberService.saveAndUpdateActiveMember(memberId);
-		log.info("저장된 회원 활동 정보: {}", savedActiveMember);
+		memberActivityService.updateLastActivityNow(memberId);
 	}
 }
