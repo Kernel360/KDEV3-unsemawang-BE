f164e6245b99367b9df84d62f55c02d5a18d58a8
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-12 13:25:23 +0900

    Feat: 웹소켓 연결 해제 이벤트 핸들 메서드 추가

--- src/main/java/com/palbang/unsemawang/chat/config/WebSocketEventListener.java
+++ src/main/java/com/palbang/unsemawang/chat/config/WebSocketEventListener.java
@@ -4,9 +4,9 @@ import java.util.Map;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
@@ -26,8 +26,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-
-	private final ActiveMemberService activeMemberRedisService;
+public class WebSocketEventListener {
+	private final ActiveMemberService activeMemberService;
 	private final SimpMessagingTemplate simpMessagingTemplate;
 	private final RedisTemplate<String, String> redisTemplate;
 
@@ -37,12 +37,12 @@ import lombok.extern.slf4j.Slf4j;
 	 */
 	@EventListener
 	public void handleSessionConnect(SessionConnectEvent event) {
+		// 회원 ID 추출
 		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-
 		String userId = (String)getSessionValue(headerAccessor, "userId");
 
 		if (userId == null) {
-			log.error("인증되지 않은 사용자의 접근입니다, " + event.getUser());
+			log.error("인증되지 않은 사용자의 접근입니다");
 			return;
 		}
 
@@ -53,33 +53,11 @@ import lombok.extern.slf4j.Slf4j;
 			.memberId(userId)
 			.status(ActiveStatus.ACTIVE_OTHERS)
 			.build();
-		activeMemberRedisService.saveActiveMember(saveRequest);
+		activeMemberService.saveActiveMember(saveRequest);
 
 		// 활동 상태 공유
 		String destination = "/topic/active/status/" + userId;
 		simpMessagingTemplate.convertAndSend(destination, NotifyActiveStatusMessage.of(true));
-	}
-
-	/**
-	 * 웹소켓 연결 해제 이벤트 핸들러
-	 * @param event
-	 */
-	@EventListener
-	public void handleSessionDisconnect(SessionDisconnectEvent event) {
-		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-
-		String userId = headerAccessor.getFirstNativeHeader("userId");
-
-		// 활동 상태 공유
-		String destination = "/topic/active/status/" + userId;
-		simpMessagingTemplate.convertAndSend(destination, NotifyActiveStatusMessage.of(false));
-
-		ActiveMemberSaveRequest saveRequest = ActiveMemberSaveRequest.builder()
-			.memberId(userId)
-			.status(ActiveStatus.INACTIVE)
-			.build();
-		activeMemberRedisService.saveActiveMember(saveRequest);
-
 	}
 
 	/**
@@ -100,10 +78,37 @@ import lombok.extern.slf4j.Slf4j;
 			log.info("조회하고자 하는 member ID: {}", memberId);
 
 			// 활동 상태 공유
-			ActiveMember activeMember = activeMemberRedisService.findActiveMemberById(memberId);
+			ActiveMember activeMember = activeMemberService.findActiveMemberById(memberId);
 			simpMessagingTemplate.convertAndSend(destination, NotifyActiveStatusMessage.of(activeMember.getStatus()
 				.getIsActive()));
 		}
+	}
+
+	/**
+	 * 웹소켓 연결 해제 핸들러
+	 * @param event
+	 */
+	@EventListener
+	public void handleSessionDisconnect(SessionDisconnectEvent event) {
+		// 회원 ID 추출
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String userId = (String)getSessionValue(headerAccessor, "userId");
+
+		if (userId == null) {
+			log.error("인증되지 않은 사용자의 접근입니다");
+			return;
+		}
+
+		// 활동 상태 공유
+		String destination = "/topic/active/status/" + userId;
+		simpMessagingTemplate.convertAndSend(destination, NotifyActiveStatusMessage.of(false));
+
+		// 활동 상태 저장
+		ActiveMemberSaveRequest saveRequest = ActiveMemberSaveRequest.builder()
+			.memberId(userId)
+			.status(ActiveStatus.INACTIVE)
+			.build();
+		activeMemberService.saveActiveMember(saveRequest);
 	}
 
 	private Object getSessionValue(StompHeaderAccessor headerAccessor, String key) {
