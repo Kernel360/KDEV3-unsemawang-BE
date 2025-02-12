f55f154369ab4fbbb3a38fb4da4224c3308458a3
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-12 13:21:53 +0900

    Feat: WebSocketAuthInterceptor 추가

---
+++ src/main/java/com/palbang/unsemawang/chat/config/WebSocketAuthInterceptor.java
@@ -0,0 +1,71 @@
+package com.palbang.unsemawang.chat.config;
+
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import com.palbang.unsemawang.jwt.JWTUtil;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class WebSocketAuthInterceptor implements HandshakeInterceptor {
+
+	private final JWTUtil jwtUtil;
+
+	public WebSocketAuthInterceptor(JWTUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+		WebSocketHandler wsHandler, Map<String, Object> attributes) {
+
+		log.info("serverHttpRequest.getPrincipal : {}", request.getPrincipal());
+
+		if (request instanceof ServletServerHttpRequest servletRequest) {
+			HttpServletRequest httpRequest = servletRequest.getServletRequest();
+			String token = extractTokenFromCookies(httpRequest);
+			log.info("token: {}", token);
+
+			if (token != null && !jwtUtil.isExpired(token)) {
+				String userId = jwtUtil.getId(token);
+				attributes.put("userId", userId); // WebSocket 세션에 사용자 ID 저장
+				log.info("attributes : {}", attributes);
+				log.info("✅ WebSocket session set userId: {}", userId); // 추가 로그
+				return true;
+			}
+		}
+		log.warn("⚠️ WebSocket authentication failed!");
+		return false; // 인증 실패 시 WebSocket 연결 차단
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response,
+		WebSocketHandler wsHandler, Exception exception) {
+		if (exception != null) {
+			log.error("WebSocket Handshake 실패: {}", exception.getMessage());
+		} else {
+			log.info("WebSocket Handshake 성공");
+		}
+	}
+
+	private String extractTokenFromCookies(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		log.info("cookies: {}", cookies);
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if ("Authorization".equals(cookie.getName())) {
+					return cookie.getValue();
+				}
+			}
+		}
+		return null;
+	}
+}
