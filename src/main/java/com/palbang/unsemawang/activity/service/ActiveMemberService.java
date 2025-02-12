4b18d45ef0005422d46a1d397da31d4ba8d05d1f
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-12 11:15:24 +0900

    Feat: 회원 활동 상태 조회 서비스 메서드 추가

--- src/main/java/com/palbang/unsemawang/activity/service/ActiveMemberService.java
+++ src/main/java/com/palbang/unsemawang/activity/service/ActiveMemberService.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Service;
 import com.palbang.unsemawang.activity.dto.ActiveMemberSaveRequest;
 import com.palbang.unsemawang.activity.entity.ActiveMember;
 import com.palbang.unsemawang.activity.repository.ActiveMemberRedisRepository;
+import com.palbang.unsemawang.common.constants.ResponseCode;
+import com.palbang.unsemawang.common.exception.GeneralException;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class ActiveMemberRedisService {
+public class ActiveMemberService {
 	private final ActiveMemberRedisRepository activeMemberRepository;
 	private static final long ACTIVE_TTL = 900L;
 
@@ -25,5 +27,12 @@ public class ActiveMemberRedisService {
 
 	public List<ActiveMember> findAllActiveMember() {
 		return (List<ActiveMember>)activeMemberRepository.findAll();
+	}
+
+	public ActiveMember findActiveMemberById(String memberId) {
+		ActiveMember activeMemberEntity = activeMemberRepository.findById(memberId).orElseThrow(
+			() -> new GeneralException(ResponseCode.NOT_EXIST_UNIQUE_NO)
+		);
+		return activeMemberEntity;
 	}
 }
