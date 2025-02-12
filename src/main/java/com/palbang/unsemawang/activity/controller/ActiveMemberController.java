811bd1a64e69ec4028ecb461efc7f4db78fb2c8b
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-12 11:27:03 +0900

    Rename: ActiveMemberService 이름 변경

--- src/main/java/com/palbang/unsemawang/activity/controller/ActiveMemberController.java
+++ src/main/java/com/palbang/unsemawang/activity/controller/ActiveMemberController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.palbang.unsemawang.activity.aop.NoTracking;
 import com.palbang.unsemawang.activity.entity.ActiveMember;
-import com.palbang.unsemawang.activity.service.ActiveMemberRedisService;
+import com.palbang.unsemawang.activity.service.ActiveMemberService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,7 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/active-member")
 public class ActiveMemberController {
-	private final ActiveMemberRedisService activeMemberService;
+	private final ActiveMemberService activeMemberService;
 
 	@NoTracking
 	@GetMapping
