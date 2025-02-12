018690a5a26a1611513fbe9125bd0f7cb52ecf5c
yschoi123 <cysdl5604@inu.ac.kr>
2025-02-12 11:27:57 +0900

    Fix: 세션 속성값 읽어서 회원 ID 읽도록 수정

--- src/main/java/com/palbang/unsemawang/activity/controller/ActiveStatusWebSocketController.java
+++ src/main/java/com/palbang/unsemawang/activity/controller/ActiveStatusWebSocketController.java
@@ -1,8 +1,9 @@
 package com.palbang.unsemawang.activity.controller;
 
-import org.springframework.messaging.handler.annotation.DestinationVariable;
+import java.util.Map;
+
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
@@ -10,31 +11,39 @@ import com.palbang.unsemawang.activity.aop.NoTracking;
 import com.palbang.unsemawang.activity.dto.ActiveMemberSaveRequest;
 import com.palbang.unsemawang.activity.dto.websocket.ChangeActiveStatusMessage;
 import com.palbang.unsemawang.activity.dto.websocket.NotifyActiveStatusMessage;
-import com.palbang.unsemawang.activity.service.ActiveMemberRedisService;
+import com.palbang.unsemawang.activity.service.ActiveMemberService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class ActiveStatusWebSocketController {
 	private final SimpMessageSendingOperations simpMessageSendingOperations;
-	private final ActiveMemberRedisService activeMemberRedisService;
+	private final ActiveMemberService activeMemberService;
 
 	@NoTracking
-	@MessageMapping("/active/status/{memberId}")
-	@SendTo("/topic/active/status/{memberId}")
-	public NotifyActiveStatusMessage trackActivity(
-		@DestinationVariable String memberId,
-		ChangeActiveStatusMessage changeActiveStatusMessage
+	@MessageMapping("/active/status")
+	public void traceMemberActivity(
+		ChangeActiveStatusMessage changeActiveStatusMessage,
+		@Header("simpSessionAttributes") Map<String, Object> sessionAttributes
 	) {
+
+		String memberId = (String)sessionAttributes.get("userId");
 
 		// 회원 마지막 활동 시간 갱신
 		ActiveMemberSaveRequest activeMemberSaveRequest = ActiveMemberSaveRequest.of(memberId,
 			changeActiveStatusMessage);
-		activeMemberRedisService.saveActiveMember(activeMemberSaveRequest);
+		activeMemberService.saveActiveMember(activeMemberSaveRequest);
+
+		// 메세지 전달할 경로 설정
+		String destination = "/topic/active/status/" + memberId;
 
 		// 회원 상태 구독중인 사용자에게 전달
 		boolean isActive = activeMemberSaveRequest.getStatus().getIsActive();
-		return NotifyActiveStatusMessage.of(isActive);
+		NotifyActiveStatusMessage notifyActiveStatusMessage = NotifyActiveStatusMessage.of(isActive);
+		simpMessageSendingOperations.convertAndSend(destination, notifyActiveStatusMessage);
 	}
+
 }
