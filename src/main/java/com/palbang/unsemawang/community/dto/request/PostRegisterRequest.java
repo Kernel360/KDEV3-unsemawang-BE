b6a05d01a2c47be70377ca66fc85c7c9ff5cc882
yr0202 <yooram0202@naver.com>
2025-02-13 21:36:44 +0900

    Fix: @NoHtml 적용

--- src/main/java/com/palbang/unsemawang/community/dto/request/PostRegisterRequest.java
+++ src/main/java/com/palbang/unsemawang/community/dto/request/PostRegisterRequest.java
@@ -2,6 +2,7 @@ package com.palbang.unsemawang.community.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.palbang.unsemawang.community.constant.CommunityCategory;
+import com.palbang.unsemawang.community.validation.NoHtml;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -23,11 +24,12 @@ public class PostRegisterRequest {
 
 	@JsonIgnore
 	private String memberId;
-
+	
 	@NotBlank(message = "제목을 입력해 주세요")
 	@Size(min = 1, max = 100, message = "제목은 100자 이내여야 합니다")
 	private String title;
 
+	@NoHtml
 	@NotBlank(message = "내용을 입력해 주세요")
 	@Size(min = 1, max = 10000, message = "내용은 10,000자 이내여야 합니다")
 	private String content;
